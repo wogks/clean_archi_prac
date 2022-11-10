bad0984db240e82b66199d4c3fc545f5e8552fd9
wogks <wogks27@naver.com>
2022-11-11 02:56:57 +0900

    Feat: 기능 클래스 정의

---
+++ lib/data/repository/note_repository_imple.dart
@@ -0,0 +1,35 @@
+import 'package:clean_archi_memo/domain/model/note.dart';
+import 'package:clean_archi_memo/domain/repository/note_repository.dart';
+
+class NoteRepositoryImple implements NoteRepository {
+  @override
+  Future<void> deleteNote(Note note) {
+    // TODO: implement deleteNote
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Note> getNoteById(int id) {
+    // TODO: implement getNoteById
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Note>> getNotes() {
+    // TODO: implement getNotes
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> insertNote(Note note) {
+    // TODO: implement insertNote
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> updateNote(Note note) {
+    // TODO: implement updateNote
+    throw UnimplementedError();
+  }
+
+}
