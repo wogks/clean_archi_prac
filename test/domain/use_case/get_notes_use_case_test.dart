d490653ef1ab2b712d1493b7256a488bd5fe8ac7
wogks <wogks27@naver.com>
2022-11-14 15:43:57 +0900

    Feat: mockito 활용 테스트

---
+++ test/domain/use_case/get_notes_use_case_test.dart
@@ -0,0 +1,23 @@
+import 'package:clean_archi_memo/domain/model/note.dart';
+import 'package:clean_archi_memo/domain/repository/note_repository.dart';
+import 'package:clean_archi_memo/domain/use_case/get_note_use_case.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'get_notes_use_case_test.mocks.dart';
+
+@GenerateMocks([NoteRepository])
+void main() {
+  test('정렬기능이 잘 동작해야 한다', () async {
+    final repository = MockNoteRepository();
+    final useCase = GetNoteUseCase(repository);
+
+    when(repository.getNotes()).thenAnswer((_) async => [
+          Note(timestamp: 1, title: 'tile', color: 1, content: 'content'),
+          Note(timestamp: 2, title: 'tile2', color: 2, content: 'content2'),
+        ]);
+
+        List<Note> result = await getNotes();
+  });
+}
